ef727148d90debe2e372f27cbd44fecfbdf7fc33
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-08-12 11:16:13 +0100

    Set up Heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,7 +3,7 @@ require 'dm-validations'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 require_relative './models/link'
 require_relative './models/tag'
